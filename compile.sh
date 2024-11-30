c268752ddf4afa3830801ceb199be7a9cf487ee8
Nikhil Sathe <nikhilsathe32@gmail.com>
2024-11-30 17:41:02 +0530

    removed translatf from clouds and car

---
+++ compile.sh
@@ -0,0 +1,6 @@
+g++ BirthdayBoy.cpp -o your_program -lglut -lGL -lGLU -lm
+#gcc graphic-redisplay.c -o your_program -lglut -lGL -lGLU
+#gcc branched.c -o your_program -lglut -lGL -lGLU -lm
+./your_program
+
+
